9b633e151bb11e8a7d4b041615f7d682d24f6cb1
Lambd0x <lambdox@gmail.com>
2018-11-19 20:38:12 -0200

    Random fixed

--- sat.go
+++ sat.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"time"
-    "math/rand"
-	"regexp"
-    "os"
 	"bufio"
-    "log"
-    "strings"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"regexp"
 	"strconv"
+	"strings"
+	"time"
 )
 
-var TAM int = 250
-var N int = 250000
-var T0 float32 = 1.0
-var TN float32 = 0.9999
+const TAM int = 250
+const N int = 250000
+const T0 float32 = 1.0
+const TN float32 = 0.9999
 
 //generate random number in a range (x,y)
 func random(min, max int) int {
-    rand.Seed(time.Now().UTC().UnixNano())
-	return rand.Intn(max - min) + min
+	rand.Seed(time.Now().UTC().UnixNano())
+	return rand.Intn(max)
 }
 
 //generate random initial solution
@@ -28,51 +28,49 @@ func RandomList(x int) []int {
 	var list []int
 	i := 0
 	for i < x {
-		list = append(list, random(0 , 2))
+		list = append(list, random(0, 2))
+		i++
 	}
 	return list
 }
 
 //read file with the coditions
-func read(filename string) [][]int{
-	
-    file, err := os.Open(filename)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+func read(filename string) [][]int {
 
-    scanner := bufio.NewScanner(file)
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
 	var validString = regexp.MustCompile(`(\-?[0-9]+)\s+(\-?[0-9]+)\s+(\-?[0-9]+) 0`)
 	var l [][]int
 	var s2 []int
-    for scanner.Scan() {
-		if(validString.MatchString(scanner.Text())){
+	for scanner.Scan() {
+		if validString.MatchString(scanner.Text()) {
 			s := strings.Split(validString.FindString(scanner.Text()), " ")
 			s = s[:3]
-			for _, i := range s{
+			for _, i := range s {
 				j, err := strconv.Atoi(i)
-				if err != nil{
+				if err != nil {
 					panic(err)
 				}
-				s2 = append(s2,j)
+				s2 = append(s2, j)
 			}
-			l = append(l,s2)
+			l = append(l, s2)
 			s2 = s2[:0]
 		}
-    }
-	
+	}
+
 	return l
 }
 
-func energy(initialList []int , )
-
+//func energy(initialList []int)
 
 func main() {
 	// To run a code do: go run yourfile.go
 	// To compile and be able to run from any machine do: go install and execute the generated binary over the bin folder
-	fmt.Println("Hello world")
-
-
-
+	list := RandomList(10)
+	fmt.Printf("%v", list)
 }
